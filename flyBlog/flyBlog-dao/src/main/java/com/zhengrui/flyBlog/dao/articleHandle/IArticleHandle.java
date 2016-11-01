1e24ffa9baa44f36605f2bf6987cbdd7c92db2ab
zhengrui-ds(郑瑞.信息技术中心.EC开发部) <zhengrui-ds@ds.gome.com.cn>
2016-11-01 17:45:33 +0800

    add web,facade,facadeImpl,,service

---
+++ flyBlog/flyBlog-dao/src/main/java/com/zhengrui/flyBlog/dao/articleHandle/IArticleHandle.java
@@ -0,0 +1,26 @@
+package com.zhengrui.flyBlog.dao.articleHandle;
+
+import java.util.List;
+
+import com.zhengrui.flyBlog.model.body.Article;
+
+public interface IArticleHandle {
+	
+	
+	//返回单类文章列表
+	public List<Article> selectTitleByLang(String lang);
+	//根据文章标题返回文章内容
+	public Article selectContentByTitle(String title);
+	//返回某品类的第一篇文章
+	public Article getFirstTitleBylang(String lang);
+	
+	//写入文章
+	public void addArticle(Article article);
+	
+	//根据文章标题删除文章
+	public void deleteArticleByTitle(String title);
+	
+	//根据标题更新文章
+	public Article updateArticleByTitle(Article article);
+	
+}
--- flyBlog/flyBlog-service/src/main/java/com/zhengrui/flyBlog/service/services/IarticleHandleService.java
+++ flyBlog/flyBlog-service/src/main/java/com/zhengrui/flyBlog/service/services/IarticleHandleService.java
@@ -1,12 +1,12 @@
-package com.zhengrui.flyBlog.dao.articleHandle;
+package com.zhengrui.flyBlog.service.services;
 
 import java.util.List;
 
 import com.zhengrui.flyBlog.model.body.Article;
-
-public interface ItArticleHandle {
-	
-	
+/*
+ * 该层主要进行事务处理，可将多条sql操作封装在一个函数中，或者對存入前和取出後的數據進行預處理，緩存操作也在該層中進行
+*/
+public interface IarticleHandleService {
 	//返回单类文章列表
 	public List<Article> selectTitleByLang(String lang);
 	//根据文章标题返回文章内容
@@ -22,5 +22,4 @@ public interface ItArticleHandle {
 	
 	//根据标题更新文章
 	public Article updateArticleByTitle(Article article);
-	
 }
